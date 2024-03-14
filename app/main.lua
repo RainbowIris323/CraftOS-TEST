0b08c1e9bc0ea41c345d53eb758b40f78b353680
Iris Rose Spear <108814359+RainbowIris323@users.noreply.github.com>
2024-03-14 14:47:50 -0700

    test

--- app/main.lua
+++ app/main.lua
@@ -2,6 +2,5 @@ local PrimeUI = require("/lib/PrimeUI")
 local files = require("/lib/files")
 
 PrimeUI.clear()
-PrimeUI.label(term.current(), 3, 5, files.readJson("/config/installs.json"))
-PrimeUI.borderBox(term.current(), 4, 7, 40, 1)
+PrimeUI.label(term.current(), 3, 5, "Hello!", colors.lime, colors.gray)
 PrimeUI.run()
