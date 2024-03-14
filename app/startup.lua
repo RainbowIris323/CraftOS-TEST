1938860454431c72f919c1018e76d0e69e73d85a
Iris Rose Spear <108814359+RainbowIris323@users.noreply.github.com>
2024-03-14 14:18:06 -0700

    test

--- app/startup.lua
+++ app/startup.lua
@@ -1,6 +1,6 @@
 if (fs.exists("update.lua")) then
     local file = fs.open("update.lua", "r")
-    local data = file.read()
+    local data = file.readAll()
     file.close()
     print(data)
     print(http.get("http://raw.githubusercontent.com/RainbowIris323/CraftOS-TEST/main/app/update.lua").readAll())
