dc25f2b1611deb1da9a26f81274583015467ef62
Iris Rose Spear <108814359+RainbowIris323@users.noreply.github.com>
2024-03-14 19:16:59 -0700

    moo

---
+++ app/lib/files.lua
@@ -0,0 +1,62 @@
+require("/lib/json")
+
+local files = {}
+
+function files.read (path)
+    local file = fs.open(path, 'r')
+    local content = file.readAll()
+    file.close()
+    return content
+end
+
+function files.write (path, data) 
+    local file = fs.open(path, 'w')
+    file.write(data)
+    file.close()
+end
+
+function files.modify (path, callback)
+    local content = callback(read(path))
+    local file = fs.open(path, 'w')
+    file.write(content)
+    file.close()
+    return content
+end
+
+function files.readJson (path)
+    return decode(read(path))
+end
+
+function files.modifyJson (path, callback)
+    local content = callback(readJson(path))
+    local file = fs.open(path, 'w')
+    file.write(encode(content))
+    file.close()
+    return content
+end
+
+function files.downloadFile (path, URL)
+    local file = fs.open(path, "w")
+    file.write(http.get(URL).readALL())
+    file.close()
+end
+
+function files.ensureFile (path, URL)
+    if (fs.exists(path) == false) then
+        downloadFile(path, URL)
+        return "downloaded"
+    end
+    return "allready exists"
+end
+
+function files.syncFile (path, URL)
+    if (read(path) == http.get(URL).readALL()) then return "not changed (file is up to date)" end
+    if (fs.exists(path) == false) then
+        downloadFile(path, URL)
+        return "downloaded"
+    end
+    downloadFile(path, URL)
+    return "updated"
+end
+
+return files
---
+++ app/main.lua
@@ -0,0 +1,3 @@
+local screen = require("/lib/screen")
+
+screen.draw()
