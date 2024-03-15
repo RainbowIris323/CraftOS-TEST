de34f7c87f42408e915532c71ea6fa5cbeb0dff9
Iris Rose Spear <108814359+RainbowIris323@users.noreply.github.com>
2024-03-14 20:07:43 -0700

    Automatic Commit

--- app/lib/screen.lua
+++ app/lib/screen.lua
@@ -7,7 +7,6 @@ screen.draw = function ()
     local termWidth, termHeight = term.getSize()
     
     -- Background
-    PrimeUI.borderBox(term.current(), 1, 1, termWidth - 1, termHeight - 1, colors.gray, colors.lightGray)
 
     -- Details
     PrimeUI.borderBox(term.current(), 1, 1,                                             termWidth - 1, math.floor(termHeight / 20) - 1, colors.gray, colors.gray)
